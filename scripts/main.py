6611d89c7ef75a1d47c99d051d01cd530a1bf924
Genevieve Stein-O'Brien <gsteinobrien@genevieves-mbp.win.ad.jhu.edu>
2022-08-18 17:09:47 -0400

    fix missing PMID

--- scripts/main.py
+++ scripts/main.py
@@ -140,7 +140,7 @@ def renderPage(pageName,**kwargs):
     print(template.render(**kwargs),file=outHandle)
 
 def nameBoldPubs(string):
-    return(re.sub('Stein\-O\'Brien,? G\.? ?[L]?\.?,?','<span class="font-weight-bold" style="font-size: 1.0rem"><u>Stein-O\'Brien GL</u></span>,',string))
+    return(re.sub('Stein-O\'Brien,? G\.? ?[L]?\.?,?','<span class="font-weight-bold" style="font-size: 1.0rem"><u>Stein-O\'Brien GL</u></span>,',string))
 
 env.filters['nameBoldPubs'] = nameBoldPubs
 
